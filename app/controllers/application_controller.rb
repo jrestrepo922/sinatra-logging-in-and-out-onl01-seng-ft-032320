027a921edc4529ff936281f765c0b6c34e521b3d
JUAN M RESTREPO <tinto922@gmail.com>
2020-05-05 17:36:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,11 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @user = User.find(session[:user_id])
+    if @user
     erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
