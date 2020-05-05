f6f58030ecc8cdefd54445b8ddf3bb57df18d255
JUAN M RESTREPO <tinto922@gmail.com>
2020-05-05 14:38:26 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,7 +5,8 @@ class Helpers
 
   def self.is_logged_in?(session_hash)
     binding.pry
-    !!session_hash[:user_id]
+    !!session_hash[:user_id] #if it contains a user_id is true and false if it does not
+
     # if self.current_user(session_hash)
     #   true
     # else
