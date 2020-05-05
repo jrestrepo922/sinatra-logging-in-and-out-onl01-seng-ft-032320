d9047344e723aa1b2c76a213e3e1fbd4d98481d8
JUAN M RESTREPO <tinto922@gmail.com>
2020-05-05 14:20:44 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,7 +3,11 @@ class Helpers
     @user = User.find_by(user_id: sessionn_hash[:user_id])
   end
 
-  def self.is_logged_in?
-
+  def self.is_logged_in?(session_hash)
+    if self.current_user(session_hash)
+      true
+    else 
+      false
+    end 
   end
 end
